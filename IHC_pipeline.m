c2a1895e25174aa0c099c68085eabbbca1f68ee9
unknown <portoska@gmail.com>
2022-05-19 16:32:37 +0100

    Small bug fixes in deconvolution

--- IHC_pipeline.m
+++ IHC_pipeline.m
@@ -2,9 +2,10 @@
 % @author: pdzialecka
 
 %% Deconvolve all files inside a folder
-main_folder = 'C:\Users\Pat\OneDrive - Imperial College London\AD_TI_hipp\Analysis\IHC';
-folder = fullfile(main_folder,'images');
+cohort_folder = 'Cohort_2_AD-Hipp28_29_32_35';
 
+main_folder = 'C:\Users\Pat\Desktop';
+folder = fullfile(main_folder,cohort_folder,'IHC\Images');
 files = dir(fullfile(folder,'*.svs'));
 
 deconvolve_full(files);
--- deconvolve_full.m
+++ deconvolve_full.m
@@ -1,4 +1,4 @@
-function deconvolve_full(files)
+function deconvolve_full(files,save_images)
     %% Deconvolve IHC images to separate H and DAB channels
     % @author: pdzialecka
     
@@ -10,11 +10,10 @@ function deconvolve_full(files)
     % MIJI and IJ which can be used in further scripts to access plugins
     % available in Fiji
     
-    %%
-    close all
-    clear all
-    
     %% Input variables
+    if ~exist('save_images','var')
+        save_images = 1;
+    end
 
     %% Load libraries
     addpath(genpath('Libraries'));
@@ -24,7 +23,9 @@ function deconvolve_full(files)
     javaaddpath 'C:\Program Files\MATLAB\R2021a\java\jar\mij.jar'
     javaaddpath 'C:\Users\Pat\Desktop\Fiji.app\jars\ij-1.53q.jar'
     addpath(genpath('C:\Users\Pat\Desktop\Fiji.app\scripts'));
-
+    
+    %% Process all files in the folder
+    
     %% Start fiji
     Miji;
     IJ = ij.IJ;
@@ -37,70 +38,86 @@ function deconvolve_full(files)
     % MIJ.setupExt('C:\Program Files\MATLAB\R2021a\java\jar');
 
     %%
-    for idx = 1:length(files)
-        %%
-        file = files(idx).name;
-        folder = files(idx).folder;
-%         cd(folder)
+    try
+        for idx = 1:length(files)
+            %% File info
+            file = files(idx).name;
+            folder = files(idx).folder;
+    %         cd(folder)
+    
+            %% Prepare save folder
+            save_folder = fullfile(folder,'Processed');
+            if ~exist(save_folder)
+                mkdir(save_folder);
+            end
 
-        %% Load RGB svs file
-        fname = fullfile(folder,file);
-        image_RGB = imread(fname);
+            %% Load RGB svs file
+            fname = fullfile(folder,file);
+            image_RGB = imread(fname);
 
-        % Save as tiff
-        % imwrite(a,strcat(fname(1:end-4),'.tif'));
+            % Save as tiff
+            % imwrite(a,strcat(fname(1:end-4),'.tif'));
 
-        %% Open file in Fiji
-        imp = copytoImagePlus(image_RGB);
-        imp.show();
+            %% Open file in Fiji
+            imp = copytoImagePlus(image_RGB);
+            imp.show();
 
-        % MIJ.createColor('img_small') % doesn't work for an unknown reason
-        % MIJ.createImage(img_small); % grayscale only
+            % MIJ.createColor('img_small') % doesn't work for an unknown reason
+            % MIJ.createImage(img_small); % grayscale only
 
-        %% Color deconvolution in Fiji
-        MIJ.run("RGB Color");
-        imp.close();
+            %% Color deconvolution in Fiji
+            MIJ.run("RGB Color");
+            imp.close();
 
-        MIJ.run("Colour Deconvolution", "vectors=[H DAB]");
+            MIJ.run("Colour Deconvolution", "vectors=[H DAB]");
 
-        %% Save results
-        % Save combined tif file
-        MIJ.selectWindow("new (RGB)")
-        fname_tif = strcat(fname(1:end-4),'.tif');
-        if save_files
-            IJ.save(fname_tif)
+            %% Save results
+            % Save combined tif file
+            MIJ.selectWindow("new (RGB)")
+            if save_images
+                fname_1 = fullfile(save_folder,strcat(file(1:end-4),'.tif'));
+%                 fname_1 = strcat(fname(1:end-4),'.tif');
+                IJ.save(fname_1)
+            end
+            MIJ.run("Close")
+
+
+            % this ensures file saved as 8-bit
+            MIJ.selectWindow("Colour Deconvolution")
+            MIJ.run("Close")
+
+
+            % Save deconvolved images as a stack
+            MIJ.run("Images to Stack", "use");
+            MIJ.run("Grays");
+            % MIJ.run("Delete Slice"); % remove color info slice -> this will save img as RGB
+
+            if save_images
+                fname_2 = fullfile(save_folder,strcat(file(1:end-4),'_deconv.tif'));
+                IJ.save(fname_2)
+            end
+            
+            %% Alternatively, send images back to Matlab and save from here
+            % very slow for large images (full frame)
+            % image_h = MIJ.getImage("new (RGB)-(Colour_1)");
+            % image_dab = MIJ.getImage("new (RGB)-(Colour_2)");
+            % image_res = MIJ.getImage("new (RGB)-(Colour_3)");
+
+            % image_h = uint8(image_h);
+            % image_dab = uint8(image_dab);
+            % image_res = uint8(image_res);
+
+            %% Close all windows
+            MIJ.closeAllWindows;
         end
-        MIJ.run("Close")
-
-
-        % this ensures file saved as 8-bit
-        MIJ.selectWindow("Colour Deconvolution")
-        MIJ.run("Close")
-
-
-        % Save deconvolved images as a stack
-        MIJ.run("Images to Stack", "use");
-        MIJ.run("Grays");
-        % MIJ.run("Delete Slice"); % remove color info slice -> this will save img as RGB
-
-        fname_deconv_tif = strcat(fname(1:end-4),'_deconv.tif');
-        IJ.save(fname_deconv_tif)
-
-        %% Alternatively, send images back to Matlab and save from here
-        % very slow for large images (full frame)
-        % image_h = MIJ.getImage("new (RGB)-(Colour_1)");
-        % image_dab = MIJ.getImage("new (RGB)-(Colour_2)");
-        % image_res = MIJ.getImage("new (RGB)-(Colour_3)");
-
-        % image_h = uint8(image_h);
-        % image_dab = uint8(image_dab);
-        % image_res = uint8(image_res);
-
-        %% Close all windows
-        MIJ.closeAllWindows;
-    end
 
     %% Close Fiji
     MIJ.exit;
-
+    
+    catch
+        MIJ.closeAllWindows;
+        MIJ.exit;
+    end
+    
 end
+
